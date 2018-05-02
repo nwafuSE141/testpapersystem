c420d212b5cb85a3981b5c4b69bab0148e566cee
tinchiway <hebeitianzhiwei@outlook.com>
2018-05-02 10:52:01 +0800

    more front

---
+++ front/src/components/Count.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>{{ count }}</div>
+</template>
+<script>
+  export default {
+    name:"count",
+    data(){
+      return{
+        count:0
+      };
+    },
+    methods:{
+      increment(state){
+        this.count++
+      }
+    }
+  }
+</script>
+<style>
+
+</style>
